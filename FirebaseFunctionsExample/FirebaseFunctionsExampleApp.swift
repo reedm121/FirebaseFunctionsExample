33621928183d47d2ead01df301560099e1e47a65
Reed Gantz <reedm121@gmail.com>
2024-11-26 15:20:29 -0500

    initial commit

--- FirebaseFunctionsExample/FirebaseFunctionsExampleApp.swift
+++ FirebaseFunctionsExample/FirebaseFunctionsExampleApp.swift
@@ -6,9 +6,14 @@
 //
 
 import SwiftUI
+import Firebase
 
 @main
 struct FirebaseFunctionsExampleApp: App {
+    init() {
+        FirebaseApp.configure()
+    }
+    
     var body: some Scene {
         WindowGroup {
             ContentView()
